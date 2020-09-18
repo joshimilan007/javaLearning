55125fd3e81da031039e6f3a69a78ddc2fa9c186
Milan Joshi <milan.joshi@soprasteria.com>
2020-09-18 08:02:54 +0530

    new change

---
+++ src/Programs/Student.java
@@ -0,0 +1,10 @@
+package Programs;
+
+public class Student {
+
+	public static void main(String[] s)
+
+	{
+		System.out.println("student");
+	}
+}
--- src/Programs/WhileExample.java
+++ src/Programs/WhileExample.java
@@ -8,4 +8,10 @@ public static void main(String[] args) {
     i++;  
     }  
 }  
+
+	public static void team() {
+		System.out.println("team");
+	}
+
+
 }  

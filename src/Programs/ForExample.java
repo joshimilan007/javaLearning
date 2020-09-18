fa5d0c3029bb0c12c6a5c87f845d86183a0de32b
joshimilan007 <71456616+joshimilan007@users.noreply.github.com>
2020-09-18 08:40:22 +0530

    new hi all change

--- src/Programs/ForExample.java
+++ src/Programs/ForExample.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
     //Code of Java for loop  
     for(int i=1;i<=10;i++){  
         System.out.println(i);  
+        System.out.println("Hi all");  
     }  
 }  
 }  
